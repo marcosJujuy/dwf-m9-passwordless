34fac43f0a9fe8b55745d1b6b89a6adde1c05b73
Marcos Mamani <imam2g1006@gmail.com>
2023-05-22 22:47:13 -0300

    first commit

---
+++ pages/api/index.ts
@@ -0,0 +1,6 @@
+import type  { NextApiRequest, NextApiResponse} from "next";
+// import {} from "lib/firestore";
+
+export default function (req: NextApiRequest, res: NextApiResponse) {
+    res.send("hola soy la api " + process.env.FIREBASE_CONNECTION);
+}
